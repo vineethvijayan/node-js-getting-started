36d031fd65c54eda0137309dbd47acb5957b50ba
vineethvijayan <vineeth.v@naicoits.com>
2021-07-31 21:08:55 +0530

    get users

--- index.js
+++ index.js
@@ -10,7 +10,44 @@ app.get('/', (req, res) => {
 })
 
 app.get('/getUser', (req, res) => {
-  res.send('hello world')
+
+  const csv = require('csv-parser');
+  const fs = require('fs');
+  const fastcsv = require('fast-csv');
+
+  var data = [];
+
+  fs.createReadStream('data.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      console.log(row)
+      data.push(row)
+    })
+    .on('end', () => {
+      console.log('CSV file successfully processed')
+      
+      var firstItem = data[0];
+
+      // remove first item in array
+      data.shift()
+
+      // write to csv file
+      // fs.writeFile('data.csv', data, (err) => {
+      //   if (err) throw err;
+      //   console.log('The file has been saved!');
+      // })
+      console.log('firstItem')
+      console.log(firstItem)
+
+      const ws = fs.createWriteStream("data.csv");
+      console.log('firstItem 1')
+      fastcsv
+        .write(data, { headers: true })
+        .pipe(ws);
+
+      res.send(firstItem)
+    });
+  
 })
 
 
